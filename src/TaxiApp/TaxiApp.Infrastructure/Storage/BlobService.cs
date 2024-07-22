307c1bc315000b3769f0afa63a20ac6fa7d9b769
urosCogi987 <urosworkonly@gmail.com>
2024-07-22 22:34:32 +0200

    Azure blob storage initial upload implementation

--- src/TaxiApp/TaxiApp.Infrastructure/Storage/BlobService.cs
+++ src/TaxiApp/TaxiApp.Infrastructure/Storage/BlobService.cs
@@ -1,22 +1,46 @@
-﻿using TaxiApp.Application.Abstractions;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using TaxiApp.Application.Abstractions;
 
 namespace TaxiApp.Persistence.Storage
 {
-    internal sealed class BlobService : IBlobService
+    public sealed class BlobService(BlobServiceClient blobServiceClient) : IBlobService
     {
-        public Task DeleteAsync(Guid fileId, CancellationToken cancellationToken = default)
+        private const string containerName = "profile-pictures";
+
+        public async Task DeleteAsync(Guid fileId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
+
+            await blobClient.DeleteIfExistsAsync(cancellationToken: cancellationToken);
         }
 
-        public Task<FileResponse> DownloadAsync(Guid fileId, CancellationToken cancellationToken = default)
+        public async Task<FileResponse> DownloadAsync(Guid fileId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
+
+            Azure.Response<BlobDownloadResult> response = await blobClient.DownloadContentAsync(cancellationToken: cancellationToken);
+
+            return new FileResponse(response.Value.Content.ToStream(), response.Value.Details.ContentType); // can be null
         }
 
-        public Task<Guid> UploadAsync(Stream stream, string contentType, CancellationToken cancellationToken = default)
+        public async Task<Guid> UploadAsync(Stream stream, string contentType, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            var fileId = Guid.NewGuid();
+            BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
+
+            await blobClient.UploadAsync(
+                stream,
+                new BlobHttpHeaders { ContentType = contentType }, 
+                cancellationToken: cancellationToken);
+
+            return fileId;
         }
     }
 }
