e8f9b08904c108233d958edaedcb0d4522ac3b47
urosCogi987 <urosworkonly@gmail.com>
2024-08-06 13:13:15 +0200

    Blocked user check.

--- src/TaxiApp/TaxiApp.Application/Drive/Commands/Accept/AcceptDriveCommandHandler.cs
+++ src/TaxiApp/TaxiApp.Application/Drive/Commands/Accept/AcceptDriveCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using TaxiApp.Application.Abstractions;
+using TaxiApp.Domain.Entities.Enum;
 using TaxiApp.Domain.Repositories;
 using DriveEntity = TaxiApp.Domain.Entities.Drive;
 
@@ -7,16 +8,27 @@ namespace TaxiApp.Application.Drive.Commands.Accept
 {
     internal sealed class AcceptDriveCommandHandler(
         IUserContext userContext,
-        IDriveRepository driveRepository) : IRequestHandler<AcceptDriveCommand>
+        IDriveRepository driveRepository,
+        IUserRepository userRepository) : IRequestHandler<AcceptDriveCommand>
     {
         public async Task Handle(AcceptDriveCommand request, CancellationToken cancellationToken)
-        {
-            //
-            //User? user = await userRepository.GetItemByIdAsync(request.UserId);
+        {            
+            if (!userContext.IsAuthenticated)
+            {
+                throw new ApplicationException("User is not authenticated");
+            }
+
+            var user = await userRepository.GetItemByIdAsync(userContext.UserId);
+            if (user?.UserStatus == UserStatus.Blocked)
+            {
+                throw new ApplicationException("User is blocked");
+            }
 
             DriveEntity? drive = await driveRepository.GetItemByIdAsync(request.DriveId);
             if (drive is null)
+            {
                 throw new ApplicationException("Drive does not exist");
+            }                
 
             drive.AcceptDrive(userContext.UserId, request.DriverArrivingTime);            
 
